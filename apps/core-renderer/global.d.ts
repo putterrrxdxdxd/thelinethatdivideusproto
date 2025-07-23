0ba6766728e3dbf5d85ae31c00d0bae7107aa9df
putterrrxdxdxd <mightyputter16743@gmail.com>
2025-07-23 23:28:56 +0700

    fix(core-renderer): add fallback module declarations for all plugins to silence TypeScript errors in CI/CD

--- apps/core-renderer/global.d.ts
+++ apps/core-renderer/global.d.ts
@@ -1,4 +1,8 @@
 declare module '@theline/plugins-dragdrop';
 declare module '@theline/plugins-interact';
 declare module '@theline/plugins-filters';
-declare module '@theline/plugins-gas'; 
+declare module '@theline/plugins-gas';
+declare module '@theline/plugins-audio';
+declare module '@theline/plugins-video';
+declare module '@theline/plugins-image';
+declare module '@theline/plugins-webcam'; 
