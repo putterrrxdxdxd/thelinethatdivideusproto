272dab25a0bd8d4c233b8cae48fef49c05567f16
putterrrxdxdxd <mightyputter16743@gmail.com>
2025-07-23 21:40:26 +0700

    fix(shared): add type-only index.d.ts entrypoint to ensure dist/index.d.ts is always emitted for consumers and CI/CD

--- packages/shared/src/index.d.ts
+++ packages/shared/src/index.d.ts
@@ -1,6 +1,5 @@
-import './types';
-export * from './api.js';
-export * from './utils.js';
-export * from './socket.js';
-export * from './state.js';
-export * from './types.js';
+export * from './api';
+export * from './utils';
+export * from './socket';
+export * from './state';
+export * from './types';
