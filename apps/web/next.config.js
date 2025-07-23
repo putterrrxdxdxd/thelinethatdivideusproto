7fe567f8da330f06498f2bebcb45dcb2aea0e8ee
putterrrxdxdxd <mightyputter16743@gmail.com>
2025-07-23 23:45:32 +0700

    fix(web): use next-transpile-modules for local plugin/shared resolution in Next.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withTM = require('next-transpile-modules')([
+  '@theline/shared',
+  '@theline/plugins-audio',
+  '@theline/plugins-dragdrop',
+  '@theline/plugins-filters',
+  '@theline/plugins-gas',
+  '@theline/plugins-image',
+  '@theline/plugins-interact',
+  '@theline/plugins-video',
+  '@theline/plugins-webcam'
+]);
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ...your existing config
+};
+
+module.exports = withTM(nextConfig);
